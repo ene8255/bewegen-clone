c3b11b4b6fd85b428bd781f3785252d34236e305
ene8255 <86288109+ene8255@users.noreply.github.com>
2022-02-22 22:51:56 +0900

    text slider one 완성

--- js/details.js
+++ js/details.js
@@ -5,9 +5,9 @@ const mainID = document.querySelector('#mainImgDiv');
 document.addEventListener('scroll', () => {
     const sct = document.documentElement.scrollTop;
     const ostMD = mainDetail.offsetTop;
-    const oshMD = mainDetail.offsetHeight;
+    const bottomMD = mainDetail.getBoundingClientRect().bottom;
 
-    if(sct >= (ostMD + oshMD)) {
+    if(sct >= bottomMD) {
         // mainID.classList.remove('zoom');
         mainDetail.classList.remove('fixed');
     }else if(sct >= ostMD) {
@@ -16,5 +16,52 @@ document.addEventListener('scroll', () => {
     }else if(sct <= 100) {
         mainID.classList.remove('zoom');
         mainDetail.classList.remove('fixed');
+    }
+})
+
+
+// carousel
+let currentOne = 0;
+
+const tOne = document.querySelector('#t-carousel-one');
+const tOneLis = document.querySelectorAll('#t-carousel-one > li');
+const tOneNav = document.querySelector('#mainDetail .carousel-nav');
+const navBtns = document.querySelectorAll('.carousel-nav > .circle-btn');
+const lines = document.querySelectorAll('.line');
+
+tOne.style.length = (tOneLis.length * 100) + '%';
+tOneLis.forEach((li, idx) => {
+    li.style.left = (idx * 100) + '%';
+})
+
+function moveSlideF() {
+    currentOne++;
+    tOne.style.left = -(currentOne * 100) + '%';
+    console.log(currentOne);
+}
+function moveSlideB() {
+    currentOne--;
+    tOne.style.left = -(currentOne * 100) + '%';
+    console.log(currentOne);
+}
+
+tOneNav.addEventListener('click', (e) => {
+    lines.forEach(line => line.classList.remove('on'));
+    const classNames = e.target.className;
+
+    if(classNames === "fa-solid fa-angle-right" || classNames === "circle-btn right") {
+        moveSlideF();
+        lines[currentOne + 1].classList.add('on');
+    }else if(classNames === "fa-solid fa-angle-left" || classNames === "circle-btn left") {
+        moveSlideB();
+        lines[currentOne + 1].classList.add('on');
+    }
+    
+    if(currentOne === 0) {
+        navBtns[0].classList.add('disabled');
+    }else if(currentOne === 7) {
+        navBtns[1].classList.add('disabled');
+    }else {
+        navBtns.forEach(btn => btn.classList.remove('disabled'));
     }
 })
